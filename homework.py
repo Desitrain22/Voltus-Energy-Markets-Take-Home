2c711521ed7e7e2b00734944a5037d456ed9482b
Neal Paresh Patel <nealpareshpatel@gmail.com>
2023-12-15 23:28:57 -0500

    initial commit -- part 1

---
+++ homework.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+miso_baselines = {1: 10700, 2: 5400, 3: 850, 5: 9000, 6: 350}
+
+
+def get_site_data_as_series(
+    file_path: str = "site_1.csv", index_header="Interval Beginning (EST)"
+):
+    series = pd.read_csv(file_path)
+    series["Interval Beginning (EST)"] = series["Interval Beginning (EST)"].apply(
+        pd.Timestamp
+    )
+    return series.set_index("Interval Beginning (EST)")
+
+
+def customer_performance_from_baseline(customer_series: pd.Series, baseline: float):
+    return (
+        customer_series.rolling(window=4)
+        .apply(sum)
+        .dropna()
+        .iloc[::4]
+        .apply(lambda x: baseline - x)
+        .mean()
+        .iloc[0]
+    )
+
+
+def get_10of10_baseline(
+    data: pd.Series = get_site_data_as_series(),
+    event_date: pd.Timestamp = pd.Timestamp("2022-06-14"),
+) -> float:
+    hourly_aggregates = data.groupby(
+        data.index.floor("h")
+    ).sum()  # groups the total recorded performance hourly (adding up all values within an hour interval)
+    daily_average = hourly_aggregates.groupby(
+        hourly_aggregates.index.floor("d")
+    ).mean()  # calculate the average hourly recorded performance over the course of each day
+    return (
+        daily_average[daily_average.index.dayofweek < 5]
+        .loc[
+            event_date
+            - (pd.offsets.BusinessDay(10)) : event_date
+            - (pd.offsets.BusinessDay(1))
+        ]
+        .mean()
+        .iloc[0]
+    )  # filters for weekends, takes the average of the 10 days prior to the event date
+
+
+for i in [1, 2, 3, 5, 6]:
+    site_data = get_site_data_as_series("site_" + str(i) + ".csv")
+    fsl = customer_performance_from_baseline(
+        site_data.loc[
+            pd.Timestamp("2022-06-14 14:00:00") : pd.Timestamp("2022-06-14 18:00:00")
+        ],
+        get_10of10_baseline(data=site_data, event_date=pd.Timestamp("2022-06-14")),
+    )
+    ten_of_ten = customer_performance_from_baseline(
+        site_data.loc[
+            pd.Timestamp("2022-06-14 14:00:00") : pd.Timestamp("2022-06-14 18:00:00")
+        ],
+        miso_baselines[i],
+    )
+    print("Site " + str(i) + ":\nFSL: ", fsl, "\n10of10: ", ten_of_ten)
